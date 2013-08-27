a20ac68dee6db00daa1bf6e43d0ad94a7c27e034
Mo McRoberts <mo@nevali.net>
2013-08-27 23:08:59 +0100

    Add tests for opening, writing, closing and unlinking.

---
+++ t/open-1.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013 Mo McRoberts.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "../fcntl.h"
+#include "../unistd.h"
+
+#include <errno.h>
+
+extern int dprintf(const char *fmt, ...);
+
+int
+main(int argc, char **argv)
+{
+	int fd;
+	const char *str = "This is a test of write()\n";
+	size_t len = 26;
+	ssize_t r;
+
+	(void) argc;
+	(void) argv;
+
+	fd = open("t/open-1.out", O_WRONLY|O_CREAT, 0777);
+	if(fd == -1)
+	{
+		dprintf("open: r = %d, errno = %d\n", fd, errno);
+		return -1;
+	}
+	/* Write str to the file */
+	r = write(fd, str, len);
+	if(r != (ssize_t) len)
+	{
+		dprintf("r = %d, errno = %d\n", (int) r, (int) errno);
+		return errno;
+	}
+	close(fd);	
+	return 0;
+}
